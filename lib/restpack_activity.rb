7934c14e7148e6ef467872a153b9367da712e7ed
Gavin Joyce <gavinjoyce@gmail.com>
2013-08-03 16:18:07 +0100

    added skeleton rspec spec

--- lib/restpack_activity.rb
+++ lib/restpack_activity.rb
@@ -1,4 +1,5 @@
-require "restpack_activity/version"
+require "active_model"
 
+require "restpack_activity/version"
 require "restpack_activity/models/page"
 require "restpack_activity/models/activity"
